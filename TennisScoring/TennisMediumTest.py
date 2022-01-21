022f2bd5aa4523d30ae60c48769e7ef798d02895
todd flanders <toddfbass@gmail.com>
2022-01-21 14:09:23 -0500

    cleanup inspection warnings in tennis tests

--- TennisScoring/TennisMediumTest.py
+++ TennisScoring/TennisMediumTest.py
@@ -1,93 +1,20 @@
 import pytest
 
 from TennisMedium import TennisGameMedium
+from TennisScoring.TennisTestData import test_data
 
 
-@pytest.mark.parametrize("p1Score, p2Score, expectedScore", [
-    (0, 0, "Love-All"),
-    (1, 1, "Fifteen-All"),
-    (2, 2, "Thirty-All"),
-    (3, 3, "Deuce"),
-    (4, 4, "Deuce"),
-
-    (1, 0, "Fifteen-Love"),
-    (0, 1, "Love-Fifteen"),
-    (2, 0, "Thirty-Love"),
-    (0, 2, "Love-Thirty"),
-    (3, 0, "Forty-Love"),
-    (0, 3, "Love-Forty"),
-    (4, 0, "Win for player1"),
-    (0, 4, "Win for player2"),
-
-    (2, 1, "Thirty-Fifteen"),
-    (1, 2, "Fifteen-Thirty"),
-    (3, 1, "Forty-Fifteen"),
-    (1, 3, "Fifteen-Forty"),
-    (4, 1, "Win for player1"),
-    (1, 4, "Win for player2"),
-
-    (3, 2, "Forty-Thirty"),
-    (2, 3, "Thirty-Forty"),
-    (4, 2, "Win for player1"),
-    (2, 4, "Win for player2"),
-
-    (4, 3, "Advantage player1"),
-    (3, 4, "Advantage player2"),
-    (5, 4, "Advantage player1"),
-    (4, 5, "Advantage player2"),
-    (15, 14, "Advantage player1"),
-    (14, 15, "Advantage player2"),
-
-    (6, 4, "Win for player1"),
-    (4, 6, "Win for player2"),
-    (1, 1, "Fifteen-All"),
-    (2, 2, "Thirty-All"),
-    (3, 3, "Deuce"),
-    (4, 4, "Deuce"),
-
-    (1, 0, "Fifteen-Love"),
-    (0, 1, "Love-Fifteen"),
-    (2, 0, "Thirty-Love"),
-    (0, 2, "Love-Thirty"),
-    (3, 0, "Forty-Love"),
-    (0, 3, "Love-Forty"),
-    (4, 0, "Win for player1"),
-    (0, 4, "Win for player2"),
-
-    (2, 1, "Thirty-Fifteen"),
-    (1, 2, "Fifteen-Thirty"),
-    (3, 1, "Forty-Fifteen"),
-    (1, 3, "Fifteen-Forty"),
-    (4, 1, "Win for player1"),
-    (1, 4, "Win for player2"),
-
-    (3, 2, "Forty-Thirty"),
-    (2, 3, "Thirty-Forty"),
-    (4, 2, "Win for player1"),
-    (2, 4, "Win for player2"),
-
-    (4, 3, "Advantage player1"),
-    (3, 4, "Advantage player2"),
-    (5, 4, "Advantage player1"),
-    (4, 5, "Advantage player2"),
-    (15, 14, "Advantage player1"),
-    (14, 15, "Advantage player2"),
-
-    (6, 4, "Win for player1"),
-    (4, 6, "Win for player2"),
-    (16, 14, "Win for player1"),
-    (14, 16, "Win for player2")
-])
-def test_tennis_score(p1Score, p2Score, expectedScore):
+@pytest.mark.parametrize("p1_score, p2_score, expected_score", test_data)
+def test_tennis_score(p1_score, p2_score, expected_score):
     game = TennisGameMedium("player1", "player2")
 
-    scorePoints(game, "player1", p1Score)
-    scorePoints(game, "player2", p2Score)
+    score_points(game, "player1", p1_score)
+    score_points(game, "player2", p2_score)
 
-    actualScore = game.get_score()
-    assert expectedScore == actualScore
+    actual_score = game.get_score()
+    assert expected_score == actual_score
 
 
-def scorePoints(game: TennisGameMedium, playerName: str, points: int):
+def score_points(game: TennisGameMedium, player_name: str, points: int):
     for i in range(points):
-        game.WonPoint(playerName);
+        game.WonPoint(player_name)
